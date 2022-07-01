2cc18d777eedd3f0814fd1fe637d357e38c56b5e
eden <85859364+eden-jpg@users.noreply.github.com>
2022-06-30 23:45:26 -0500

    updated the JS file

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
 var startBtn = document.querySelector("#start_quiz");
+var questionIndex
 let score = 0;
 let rightanswerReward = 1;
 let wronganswer = -1;
 
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("start_time");
+var question = document.querySelector("wrapper");
+
+var secondsleft= 80;
+var holdinterval = 0;
+var penalty = 20;
+
+var ulcreate = document.createElement ("ul");
+
+time.addEventListener("click", function () {
+    if (holdinterval === 0) {
+        holdinterval = setInterval(function () {
+            secondleft--; 
+            currentTime.textContent = "time:" + secondleft;
+            if (secondsleft <= 0){
+                clearInterval(holdinterval);
+                alldone();
+                currentTime.textContent = "!time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+function render(questionIndex){ 
+    question.innerHTML = "";
+    ulcreate.innerHTML = "";
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].title;
+        var userchoices = questions[questionIndex].choices;
+        question.textContent = userQuestion;
+    }
+    userchoices.foreach(function(anotherfeatuer){
+        var listfeatuer = document.createAttribute;
+        listfeatuer.textContent = anotherfeatuer; 
+        question.appendchild(ulcreate); 
+
+        listfeatuer.addEventListener("click", (compare));
+
+    })
+}
 var questions = [
     {
-        question: "bfdjxfbmd:",
-        choies: ["c1","c2","c3"],
-        answer: "c2"
+        question: "Javascript is an _____ language:",
+        choies: ["Object-Oriented","Object-Based","Procedural"],
+        answer: "Object-Oriented"
     }, 
+    {
+        question: "How can a datatype be declared to be a constant type:",
+        choies: ["var","const","let"],
+        answer: "const"
+    },
+    {
+        question: "When an operator's value is NULL, the typeof returned byt he unray operaror is:",
+        choies: ["undefined","boolean","object"],
+        answer: "object"
+    }
 
-function int
 
     
 ]
